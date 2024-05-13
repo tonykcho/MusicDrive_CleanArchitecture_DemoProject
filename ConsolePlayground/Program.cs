bc576df1793397ddb456ae5532595c435d2bf515
tonykcho <tonykcho3527657@gmail.com>
2024-05-13 01:30:35 -0400

    Set up Console Playground for testing.

---
+++ ConsolePlayground/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConsolePlayground
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
