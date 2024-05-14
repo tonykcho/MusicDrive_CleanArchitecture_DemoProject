31b132f7d8fa980b22af0ae966f46be4555d4364
tonykcho <tonykcho3527657@gmail.com>
2024-05-14 01:15:21 -0400

    Added Serilog and logging

---
+++ MusicDrive.Api/Extensions/WebApplicationBuilderExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.HttpLogging;
+using Serilog;
+using Serilog.Core;
+
+namespace MusicDrive.Api.Extensions;
+
+public static class WebApplicationBuilderExtension
+{
+    public static void ConfigureLogging(this WebApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        builder.Host.UseSerilog();
+
+        builder.Services.AddHttpLogging(logging =>
+        {
+            logging.LoggingFields = HttpLoggingFields.RequestPath | HttpLoggingFields.RequestMethod
+                | HttpLoggingFields.ResponseStatusCode | HttpLoggingFields.ResponseBody;
+            logging.RequestBodyLogLimit = 4096;
+            logging.ResponseBodyLogLimit = 4096;
+        });
+    }
+}
--- MusicDrive.Api/Program.cs
+++ MusicDrive.Api/Program.cs
@@ -1,7 +1,13 @@
+using MusicDrive.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.ConfigureLogging();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseHttpLogging();
 
 app.Run();
